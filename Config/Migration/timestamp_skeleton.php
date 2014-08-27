2b46b2434ce3e4e317179cea02eaf69abebeebdc
s-nakajima <nakajimashouhei@gmail.com>
2014-08-27 10:56:00 +0900

    Tutorial commit

--- Config/Migration/timestamp_skeleton.php
+++ Config/Migration/timestamp_skeleton.php
@@ -6,7 +6,7 @@
  * @author        SkeletonAuthorName <SkeletonAuthorEMail>
  * @link          http://www.netcommons.org NetCommons Project
  * @license       http://www.netcommons.org/license.txt NetCommons License
- * @copyright    Copyright 2014, NetCommons Project
+ * @copyright     Copyright 2014, NetCommons Project
  * @package       app.Plugin.Skeleton.Config.Migration
  */
 
